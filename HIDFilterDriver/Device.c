fc89f6d02565b412cb01394aa6702f1a1801944a
Roy Eldar <royeldar@hotmail.com>
2024-09-13 13:42:02 +0300

    Create framework filter device object

--- HIDFilterDriver/Device.c
+++ HIDFilterDriver/Device.c
@@ -4,11 +4,21 @@
 
 NTSTATUS MyEvtDeviceAdd(IN WDFDRIVER Driver, IN PWDFDEVICE_INIT DeviceInit) {
 	NTSTATUS status = STATUS_SUCCESS;
+	WDFDEVICE device;
 
 	UNREFERENCED_PARAMETER(Driver);
-	UNREFERENCED_PARAMETER(DeviceInit);
 
-	// TODO add device
+	DoTraceMessage(TRACE_DRIVER, "[%!FUNC!] Adding device...");
+
+	// Create a framework device object
+	WdfFdoInitSetFilter(DeviceInit);
+	status = WdfDeviceCreate(&DeviceInit, WDF_NO_OBJECT_ATTRIBUTES, &device);
+	if (!NT_SUCCESS(status)) {
+		DoTraceMessage(TRACE_DRIVER, "[%!FUNC!] Failed to add device (0x%08x)!", status);
+		return status;
+	}
+
+	DoTraceMessage(TRACE_DRIVER, "[%!FUNC!] Successfully added device.");
 
 	return status;
 }
