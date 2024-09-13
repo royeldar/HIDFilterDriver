df4e23de06cac51dd3f152f3d3be24d51ff6bdee
Roy Eldar <royeldar@hotmail.com>
2024-09-13 13:18:31 +0300

    Create framework driver object

---
+++ HIDFilterDriver/Device.c
@@ -0,0 +1,12 @@
+#include "Device.h"
+
+NTSTATUS MyEvtDeviceAdd(IN WDFDRIVER Driver, IN PWDFDEVICE_INIT DeviceInit) {
+	NTSTATUS status = STATUS_SUCCESS;
+
+	UNREFERENCED_PARAMETER(Driver);
+	UNREFERENCED_PARAMETER(DeviceInit);
+
+	// TODO add device
+
+	return status;
+}
--- HIDFilterDriver/Driver.c
+++ HIDFilterDriver/Driver.c
@@ -1,12 +1,21 @@
 #include "Driver.h"
+#include "Device.h"
+
+static EVT_WDF_DRIVER_UNLOAD MyEvtDriverUnload;
 
 NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING RegistryPath) {
 	NTSTATUS status = STATUS_SUCCESS;
+	WDF_DRIVER_CONFIG config;
+	WDFDRIVER driver;
 
-	UNREFERENCED_PARAMETER(DriverObject);
-	UNREFERENCED_PARAMETER(RegistryPath);
-
-	// TODO initialize driver
+	// Create a framework driver object
+	WDF_DRIVER_CONFIG_INIT(&config, MyEvtDeviceAdd);
+	config.EvtDriverUnload = MyEvtDriverUnload;
+	status = WdfDriverCreate(DriverObject, RegistryPath, WDF_NO_OBJECT_ATTRIBUTES, &config, &driver);
 
 	return status;
+}
+
+static void MyEvtDriverUnload(IN WDFDRIVER Driver) {
+	UNREFERENCED_PARAMETER(Driver);
 }
